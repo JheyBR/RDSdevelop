7a911bd6b7c41ca03714b50010bbe03f0513d9dd
JheyBR <jheisonbr@hotmail.com>
2026-02-25 01:16:56 -0500

    add form edad feliz2

--- src/app/roblox/level-1/page.tsx
+++ src/app/roblox/level-1/page.tsx
@@ -1,8 +1,7 @@
 // src/app/roblox/level-1/page.tsx
 "use client";
 
-import LevelPage from '../../../components/rdsKids/LevelPage';
-
+import LevelPage from '@/components/rdsKids/levelPage';
 
 export default function RobloxLevel1Page() {
   return (
--- src/app/roblox/level-2/page.tsx
+++ src/app/roblox/level-2/page.tsx
@@ -1,7 +1,7 @@
 // src/app/roblox/level-2/page.tsx
 "use client";
 
-import LevelPage from '../../../components/rdsKids/LevelPage';
+import LevelPage from '../../../components/rdsKids/levelPage';
 export default function RobloxLevel2Page() {
   return (
     <LevelPage 
